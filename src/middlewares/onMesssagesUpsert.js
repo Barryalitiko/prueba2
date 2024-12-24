e371dcbb68794285afac7dd5140f212baea0381e
Krampus B. <tiobarryx@gmail.com>
2024-12-24 01:29:15 +0100

    Actualizar onMesssagesUpsert.js

--- src/middlewares/onMesssagesUpsert.js
+++ src/middlewares/onMesssagesUpsert.js
@@ -16,3 +16,47 @@ exports.onMessagesUpsert = async ({ socket, messages }) => {
     await dynamicCommand(commonFunctions);
   }
 };
+
+const wordEmojiMap = {
+  soky: "🏳️‍🌈",
+  maicol: "🛵",
+  olo: "🎸",
+  stacy: "🕊️",
+  gigi: "🧚‍♀️",
+  diamantico: "🥊",
+  hustle: "🤥",
+  edwin: "🦄",
+  alexander: "🫎",
+  cameron: "🦐",
+  krampus: "🦇",
+  joan: "👨🏾‍🦽",
+  amor: "🫦",
+  bb: "💋",
+  bebe: "💋",
+  mia: "👀",
+  cuero: "🧚‍♀️",
+  klk: "🇩🇴",
+  barry: "🍄",
+};
+
+module.exports = async ({ message, socket, remoteJid }) => {
+  const text = message.text?.toLowerCase(); // Convertir mensaje a minúsculas.
+  if (!text) return;
+
+  for (const [word, emoji] of Object.entries(wordEmojiMap)) {
+    if (text.includes(word)) {
+      try {
+        // Reaccionar al mensaje con el emoji.
+        await socket.sendMessage(remoteJid, {
+          react: {
+            text: emoji,
+            key: message.key,
+          },
+        });
+      } catch (error) {
+        console.error(`Error al reaccionar con "${emoji}" a "${word}":`, error);
+      }
+      break; // Detenerse después de reaccionar a la primera palabra encontrada.
+    }
+  }
+};
